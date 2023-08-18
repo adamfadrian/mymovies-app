a85f74e68e516ed63c94a13a46d82add585f246e
fmqonik <kaniafajriya@gmail.com>
2023-08-18 11:10:29 +0700

    added Login components and pict Login assets

---
+++ src/pages/auth/Login.tsx
@@ -0,0 +1,7 @@
+import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+import axios from "axios";
+
+import { Button } from "components/Button";
+import login from "src/assets/Login.png";
+import Layout from "components/Layout";
