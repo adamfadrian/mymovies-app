f989b8020b82d78553ee40243f83501625e49e71
fmqonik <kaniafajriya@gmail.com>
2023-08-22 19:38:30 +0700

    added react-cookie in packages and modified navbar components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,23 @@
 import { FaRegHeart, FaBars, FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { useContext } from "react";
 
 import { ThemeContext } from "utils/context";
+import { AuthState, logout } from "pages/authSlice";
 
 const Navbar = () => {
   const { theme, setTheme } = useContext(ThemeContext);
+  const auth = useSelector((state: {auth: AuthState}) => state.auth);
+  const dispatch = useDispatch();
 
   function handleTheme() {
     theme === "light" ? setTheme("dark") : setTheme("light");
-  }
+  };
+
+  const handleLogout = () => {
+    dispatch(logout())
+}
 
   return (
     <div className="navbar sticky top-0 z-20 bg-zinc-300 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-300">
@@ -97,6 +105,18 @@ const Navbar = () => {
             <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
           </svg>
         </label>
+            {
+              auth.isAuth ?
+              <div className="flex flex-row gap-2 justify-center">
+                <h1>{auth.user?.username}</h1>
+                <button onClick={() => handleLogout()} className="my-auto btn btn-xs bg-white">Logout</button>
+              </div>
+              :
+              <Link to={"/login"}>
+              <button className="btn bg-transparent text-bold text-white btn-sm border-zinc-300 btn-info">Login</button>
+
+            </Link>
+            }
       </div>
       <Link to="/favorite" className="text-[1.7rem] mx-3 hidden md:flex">
         <FaRegHeart />
